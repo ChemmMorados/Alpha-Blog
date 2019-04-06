7bfe1c2ca78d25472eea4316704af248946c3b0b
Chemm Morados <chemmmoradoss@gmail.com>
2019-04-06 23:24:29 +0800

    Added Partials and delete button

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
 
 def index
-  @articles = Article.all 
+  @articles = Article.all
 end
 
 def new
@@ -34,6 +34,13 @@ end
 
 def show
 @article = Article.find(params[:id])
+end
+
+def destroy
+  @article = Article.find(params[:id])
+  @article.destroy
+  flash[:notice] = "Article was succesfully deleted!"
+  redirect_to articles_path
 end
 
 private
